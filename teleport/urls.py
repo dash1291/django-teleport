4307bee16f74547c2258ea794f8feab7ec47f324
Ashish Dubey <ashish.dubey91@gmail.com>
2013-11-06 19:10:55 +0530

    fix inside-directory uploads

--- teleport/urls.py
+++ teleport/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls.defaults import patterns
 from teleport.views import *
 
 urlpatterns = patterns('',
-    (r'^(?P<filepath>[^/]+)/$', ApiHandler.as_view()),
+    (r'^(?P<filepath>.+)/$', ApiHandler.as_view()),
 )
